e6afd16a3caded3f099894654d85ada4de46d97b
Brighton Dube <brighton@bizpilot.com>
2025-12-14 14:58:15 +0200

    Fix alembic: merge multiple heads

---
+++ backend/alembic/versions/003_merge_heads.py
@@ -0,0 +1,25 @@
+"""Merge heads
+
+Revision ID: 003_merge_heads
+Revises: 002_add_payments, 002_business_entities
+Create Date: 2025-12-14 00:00:00.000000
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "003_merge_heads"
+down_revision: Union[str, tuple[str, ...], None] = ("002_add_payments", "002_business_entities")
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
